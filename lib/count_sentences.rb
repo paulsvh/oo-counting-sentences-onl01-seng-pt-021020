d2a2753d0371e25f4f6c087e0467553a4119578f
Paul von Hagen <pvonhagen26@gmail.com>
2020-02-27 05:03:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,20 @@ class String
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = []
+    self.split(" ").collect do |i|
+      if i.end_with?("!") or i.end_with?("?") or i.end_with?(".")
+        new_array.push(i)
+      end
+    end
+    new_array.length
   end
 end
